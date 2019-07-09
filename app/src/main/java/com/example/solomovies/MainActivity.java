a40fd1fd4044b9a3d24af04690cfd0f301add449
Mário Silva <mariosilva@Marios-MacBook-Pro.local>
2019-07-09 23:13:59 +0100

    firstPush

---
+++ app/src/main/java/com/example/solomovies/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.solomovies;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private final String TAG = this.getClass().getSimpleName();
+    private TextView bestMovies;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate: started");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        bestMovies = findViewById(R.id.bestMovies);
+
+        GetBestMoviesByYear getBestMoviesByYear = new GetBestMoviesByYear();
+        getBestMoviesByYear.execute();
+
+        Log.d(TAG, "onCreate: ended");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        Log.d(TAG, "onCreateOptionsMenu() returned: " + true);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        Log.d(TAG, "onOptionsItemSelected() returned: returned");
+        return super.onOptionsItemSelected(item);
+    }
+}
